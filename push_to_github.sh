64dec7a2a09f87a5b3d65d99a00fc23fad9275e1
misterr8472 <hello@pinkpigeon.co.uk>
2020-12-18 14:45:34 +0000

    recent

--- push_to_github.sh
+++ push_to_github.sh
@@ -1,1 +1,1 @@
-git push https://'pinkpigeondocs':'DarlingzLike2Snuggle!'@github.com/pinkpigeondocs/Pink-Pigeon-Documentation.git
+git add . && git commit -m "recent" && git push https://'pinkpigeondocs':'DarlingzLike2Snuggle!'@github.com/pinkpigeondocs/Pink-Pigeon-Documentation.git
